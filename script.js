40230e6f542d7fbe0f87ba524d6410f9bc7bb834
Animesh Maiti <a.maiti770@gmail.com>
2022-12-26 13:37:03 +0530

    commit

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
-import firebase from "firebase/app";
-import "firebase/auth";
+const inputs = document.querySelectorAll(".input-field");
+const toggle_btn = document.querySelectorAll(".toggle");
+const main = document.querySelector("main");
+const bullets = document.querySelectorAll(".bullets span");
+const images = document.querySelectorAll(".image");
 
+inputs.forEach((inp) => {
+    inp.addEventListener("focus", () => {
+        inp.classList.add("active");
+    });
+    inp.addEventListener("blur", () => {
+        if (inp.value != "") return;
+        inp.classList.remove("active");
+    });
+});
 
+toggle_btn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        main.classList.toggle("sign-up-mode");
+    });
+});
+
+function moveSlider() {
+    let index = this.dataset.value;
+
+    let currentImage = document.querySelector(`.img-${index}`);
+    images.forEach((img) => img.classList.remove("show"));
+    currentImage.classList.add("show");
+
+    const textSlider = document.querySelector(".text-group");
+    textSlider.style.transform = `translateY(${-(index - 1) * 2.2}rem)`;
+
+    bullets.forEach((bull) => bull.classList.remove("active"));
+    this.classList.add("active");
+}
+
+bullets.forEach((bullet) => {
+    bullet.addEventListener("click", moveSlider);
+});
+// import firebase from "firebase/app";
+// import "firebase/auth";
 const firebaseApp = firebase.initializeApp({
     apiKey: "AIzaSyDV5ACAVylCvJLewxUZKRuvnNvqWeuZahI",
     authDomain: "auth-form-c6538.firebaseapp.com",
@@ -14,10 +51,10 @@ const firebaseApp = firebase.initializeApp({
 const db = firebaseApp.firestore();
 // Initialize Firebase Authentication and get a reference to the service
 const auth = firebaseApp.auth();
-//signup func
+// //signup func
 const signUp = () => {
-    const email = document.getElementById("").value;
-    const password = document.getElementById("").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
     //firebase code
     firebase.auth().createUserWithEmailAndPassword(email, password)
         .then((userCredential) => {
