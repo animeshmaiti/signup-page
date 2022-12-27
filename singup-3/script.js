42a0bf273da911f6a28b13052030e6f160dfb04d
Animesh Maiti <a.maiti770@gmail.com>
2022-12-27 22:01:41 +0530

    update

--- singup-3/script.js
+++ singup-3/script.js
@@ -1,50 +1,11 @@
-const inputs = document.querySelectorAll(".input-field");
-const toggle_btn = document.querySelectorAll(".toggle");
-const main = document.querySelector("main");
-const bullets = document.querySelectorAll(".bullets span");
-const images = document.querySelectorAll(".image");
-
-inputs.forEach((inp) => {
-    inp.addEventListener("focus", () => {
-        inp.classList.add("active");
-    });
-    inp.addEventListener("blur", () => {
-        if (inp.value != "") return;
-        inp.classList.remove("active");
-    });
-});
-
-toggle_btn.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        main.classList.toggle("sign-up-mode");
-    });
-});
-
-function moveSlider() {
-    let index = this.dataset.value;
-
-    let currentImage = document.querySelector(`.img-${index}`);
-    images.forEach((img) => img.classList.remove("show"));
-    currentImage.classList.add("show");
-
-    const textSlider = document.querySelector(".text-group");
-    textSlider.style.transform = `translateY(${-(index - 1) * 2.2}rem)`;
-
-    bullets.forEach((bull) => bull.classList.remove("active"));
-    this.classList.add("active");
-}
-
-bullets.forEach((bullet) => {
-    bullet.addEventListener("click", moveSlider);
-});
-const firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyDV5ACAVylCvJLewxUZKRuvnNvqWeuZahI",
     authDomain: "auth-form-c6538.firebaseapp.com",
-    databaseURL: "https://auth-form-c6538-default-rtdb.firebaseio.com",
     projectId: "auth-form-c6538",
     storageBucket: "auth-form-c6538.appspot.com",
     messagingSenderId: "731170493047",
     appId: "1:731170493047:web:9ef3636f3330ae7473ac72",
+    databaseURL: "https://auth-form-c6538-default-rtdb.firebaseio.com",
     measurementId: "G-RP0RXX70MD"
 };
 firebase.initializeApp(firebaseConfig);
